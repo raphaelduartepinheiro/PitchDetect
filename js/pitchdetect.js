cc94b9c82f7e58a8f86551e01efe89ffc31f5a16
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-01-05 02:05:16 -0200

    Testing library

--- js/pitchdetect.js
+++ js/pitchdetect.js
@@ -43,7 +43,7 @@ window.onload = function() {
 	audioContext = new AudioContext();
 	MAX_SIZE = Math.max(4,Math.floor(audioContext.sampleRate/5000));	// corresponds to a 5kHz signal
 	var request = new XMLHttpRequest();
-	request.open("GET", "../sounds/whistling3.ogg", true);
+	request.open("GET", "js/test.m4a", true);
 	request.responseType = "arraybuffer";
 	request.onload = function() {
 	  audioContext.decodeAudioData( request.response, function(buffer) { 
